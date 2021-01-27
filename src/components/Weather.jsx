d2156c0bd7e9d5c8f8e16dcfa1afaa7f35e3adb9
Nimitpamnani <C0764944@mylambton.ca>
2021-01-27 20:12:15 +0530

    fetchingApi

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,7 +6,19 @@ class Weather extends Component {
     super();
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const WeatherData = fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3b27d21014c24ea211eea9e77246b021`
+    );
+
+    WeatherData.then((response, error) => {
+      console.log(response);
+
+      response.json().then((result, err) => {
+        console.log(result.name);
+      });
+    });
+  }
 
   render() {
     return <h1> HelloWorld</h1>;
