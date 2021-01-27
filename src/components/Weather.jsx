4823a0ac9a46bf9960abe6e66363a2fd1f431357
Nimitpamnani <C0764944@mylambton.ca>
2021-01-27 19:32:24 +0530

    initalCommit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  state = {};
+  constructor(props) {
+    super();
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return <h1> HelloWorld</h1>;
+  }
+}
+
+export default Weather;
