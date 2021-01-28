80edd9853578e35b60c78f1964301e053176d411
Nimitpamnani <C0764944@mylambton.ca>
2021-01-28 17:34:49 +0530

    withFetchingService

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather></Weather>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    let locationDataArr = [];
+
+    super(props);
+    this.state = {
+      location: [
+        { city: "new york", country: "us" },
+        { city: "Delhi", country: "IN" },
+        { city: "Mumbai", country: "IN" },
+        // { city: "Mumbai", country: "IN" },
+      ],
+
+      locationData: [{ city: "", country: "" }],
+    };
+  }
+
+  componentWillMount() {
+    this.state.location.map((loca) => {
+      const weatherData = fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${loca.city},${loca.country}&appid=079b76b390ad70c628a14a9a141e5992`
+      );
+      weatherData.then((res) => {
+        res.json().then((result) => {
+          let newarray = this.state.locationData;
+          newarray.push({
+            city: result.name,
+            country: result.sys.country,
+            temp: result.main.temp,
+          });
+
+          this.setState({ locationData: newarray });
+
+          // console.log(this.state.locationData);
+        });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1> This is current weather</h1>
+
+        {this.state.locationData.map((locobj) => {
+          return <Weather weatherData={locobj.city}> </Weather>;
+        })}
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,21 +7,19 @@ class Weather extends Component {
   }
 
   componentDidMount() {
-    const WeatherData = fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3b27d21014c24ea211eea9e77246b021`
-    );
-
-    WeatherData.then((response, error) => {
-      console.log(response);
-
-      response.json().then((result, err) => {
-        console.log(result.name);
-      });
-    });
+    // const WeatherData = fetch(
+    //   `http://api.openweathermap.org/data/2.5/weather?q=new york,us&appid=3b27d21014c24ea211eea9e77246b021`
+    // );
+    // WeatherData.then((response, error) => {
+    //   console.log(response);
+    //   response.json().then((result, err) => {
+    //     console.log(result);
+    //   });
+    // });
   }
 
   render() {
-    return <h1> HelloWorld</h1>;
+    return <h1> HelloWorld {this.props.weatherData}</h1>;
   }
 }
 
