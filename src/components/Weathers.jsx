266a9a91fd4ba119d4bb7b31cc4eb586d5a1f886
Nimitpamnani <C0764944@mylambton.ca>
2021-02-04 18:53:36 +0530

    withWeatherApp

--- src/components/Weathers.jsx
+++ src/components/Weathers.jsx
@@ -2,17 +2,33 @@ import React, { Component } from "react";
 import "../index.css";
 
 class Weathers extends Component {
-  state = {};
+  state = {
+    emoji: "",
+  };
   constructor(props) {
     super();
+
+    this.returnEmoji = this.returnEmoji.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.returnEmoji(this.props.weatherData.temp);
+  }
+
+  returnEmoji(temp) {
+    if (temp < 0) {
+      this.setState({ emoji: "🥶" });
+    } else if (temp > 0) {
+      this.setState({ emoji: "🌞" });
+    }
+  }
 
   render() {
     return (
       <div className="bg-black h-48 w-48 p-2 m-4  text-white  hover:border-red-600 hover:bg-gray-500  border-4  hover:text-red-500 rounded-lg justify-between ">
-        <div className="flex justify-center"> 🥶 </div>
+        <div className="flex justify-center  h-8  text-6xl">
+          {this.state.emoji}
+        </div>
         <div className="mt-8  h-12  flex justify-center items-center ">
           {this.props.weatherData.temp} c
         </div>
